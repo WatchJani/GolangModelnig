27ba2a64d865e3991416118d193233d6ae7efad6
Janko <jankokondic84@gmail.com>
2023-01-07 20:30:05 +0100

    all

--- main.go
+++ main.go
@@ -12,5 +12,9 @@ func main() {
 
 	account := account.NewAccount(data.DataParser())
 
-	fmt.Println(account[8674652543].Currency["EUR"])
+	// fmt.Println(account[8674652543].Currency["EUR"])
+	//USD", "GBP", "EUR", "RUB", "JPY", "INR
+	for index, users := range account {
+		fmt.Printf("%d \t %s %d %d %d %d %d %d\n", index+1, users.Person.Name, users.Currency["USD"], users.Currency["GBP"], users.Currency["EUR"], users.Currency["RUB"], users.Currency["JPY"], users.Currency["INR"])
+	}
 }
